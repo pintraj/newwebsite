feb0d39c6d3301571181019123115a30f62b5903
psquares <pint.raj850@gmail.com>
2018-02-20 12:23:42 +0530

    adding some files

---
+++ footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pappu
+ * Date: 20/2/18
+ * Time: 11:27 AM
+ */
+?>
+<script src="assects/js/bootstrap.js"></script>
+<script src="assects/js/bootstrap.min.js"></script>
+<script src="assects/js/npm.js"></script>
+<script>
+src="http://code.jquery.com/jquery-3.3.1.min.js"
+integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+crossorigin="anonymous"></script>
---
+++ header.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pappu
+ * Date: 19/2/18
+ * Time: 7:15 PM
+ */
+?>
+<html>
+    <title>
+        Ravindrababu Ravula
+    </title>
+<header>
+<!--    bootstrap libraby-->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="assects/css/bootstrap.css">
+    <link rel="stylesheet" href="assects/css/main.css">
+
+<!--    fontawsome cdn-->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+<!--    google font-->
+    <link href="https://fonts.googleapis.com/css?family=Bellefair" rel="stylesheet">
+
+
+</header>
+<!--header menu starts here-->
+<section class="top-bar">
+    <div class="container-fluid">
+    <div class="top-left pull-left">
+        <i class="fas fa-envelope"></i> gate2017.ravindra@gmail.com
+        <i class="fas fa-mobile-alt"></i> +91-9676088734
+    </div>
+        <div class="topbar-right pull-right">
+            <a href="#">Login</a>
+            <a href="#">Register</a>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+</html>
